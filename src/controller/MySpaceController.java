6aa16e52b3c56ab8aad79ab5081ca2a6b1933565
Mac In Life <marie.ange.coco@gmail.com>
2018-01-22 18:18:34 +0100

    Modif visuel + GestionDocs

--- src/controller/MySpaceController.java
+++ src/controller/MySpaceController.java
@@ -1,12 +1,22 @@
 package controller;
 
+import java.net.URL;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import vue.VueFXMain;
 
 public class MySpaceController {
 	 @FXML
 	 private Button BDD;
 	 @FXML
 	 private Button GLPI;
+	 
+
+
 }
